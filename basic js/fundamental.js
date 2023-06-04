28df55cac87086cfd3c7003b1ec27efd9f22732f
Adedoyin Daniel <134150745+Nielads123@users.noreply.github.com>
2023-06-04 22:57:56 +0100

    Add files via upload

---
+++ basic js/fundamental.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 20;
+
+if(a > b){
+    console.log("a is greater than b");
+}
+else{
+    console.log("b is greater than a");
+}
+
+var passage;
+console.log(passage);
+var words = "Good news";
+console.log(words)
---
+++ basic js/jkk.js
@@ -0,0 +1,4 @@
+alert("Welcome To My Youtube Channel");
+alert(" Kindly Click The Notification Button To Suscribe To This Channel");
+alert("Follow Up To Get More Information About Us");
+alert("Thanks For Susribing To this Channel");
